da1588b991c487ed519d62139815f4a261b01490
shaikhafeez2002 <69213822+shaikhafeez2002@users.noreply.github.com>
2023-11-08 03:39:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                behavior: 'smooth',
+                top: targetElement.offsetTop,
+            });
+        }
+    });
+});
+
+// Mobile-friendly menu toggle
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
